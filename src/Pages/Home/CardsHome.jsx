9494c4dc12e48efeb22afa7b041ac70b8968fda1
Damian Roiz <damianx989@gmail.com>
2023-01-22 15:34:09 -0500

    benefits card components

--- src/Pages/Home/CardsHome.jsx
+++ src/Pages/Home/CardsHome.jsx
@@ -1,67 +1,45 @@
-import { Box, Stack, Text } from "@chakra-ui/react";
+import { Box, Image, Stack, Text } from "@chakra-ui/react";
 import React from "react";
-
-const infoCards = [
-  {
-    class: "card1",
-    title: "Qualified Professionals",
-    description:
-      "Top-notch customer service and treatment approach that is evidence based and proven to be successful",
-  },
-  {
-    class: "card2",
-    title: "Extensive Experience",
-    description:
-      "Over 20 years of combined experience, and extensive knowledge in many areas of physical and mental health",
-  },
-  {
-    class: "card3",
-    title: "Leading Technology",
-    description:
-      "Agile treatment techniques led by advanced technology to diagnose patient issues and concerns",
-  },
-  {
-    class: "card4",
-    title: "Direct Billing", //look for ways to have the word "billing in a seperate line"
-    description:
-      "Direct billing to various public and private insurance providers in addition to all MVA and WCB Claims",
-  },
-];
+import { benefitCards } from "./homeContent";
 
 export const CardsHome = () => {
   return (
     <>
       <Stack
-        className="cardshome"
         height="100vh"
-        flexDirection="column"
+        flexDirection="row"
         justifyContent="center"
         alignItems="center"
         p={0}
       >
-        {infoCards.map((card) => (
-          <Stack flexDirection="row" color="white">
+        {benefitCards.map(card => (
+          <Stack flexDirection="row" color="#fff">
             <Stack padding={10}>
               <Box
                 className={card.class}
-                width="220px"
-                height="400px"
-                padding={10}
+                w="230px"
+                h="400px"
+                pt={10}
                 pb={30}
+                bg={`center / cover no-repeat url(${card.coverImage})`}
               >
-                <Box boxSize="sm" paddingTop={10}>
-                </Box>
-                <Box display="flex" flexDirection="column">
+              <Box display="flex" flexDirection="column" >
                   <Text
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                    h={60}
+                    zIndex={1}
                     m={5}
                     letterSpacing={2}
                     fontSize="25px"
                     fontFamily="Roboto"
                     fontWeight="semibold"
-                  >
+              >
                     {card.title}
                   </Text>
                   <Text
+                    zIndex={1}
                     m={0}
                     padding={25}
                     letterSpacing="2px"
---
+++ src/Pages/Home/homeContent.jsx
@@ -0,0 +1,34 @@
+export const benefitCards = [
+  {
+    class: "card1",
+    coverImage:
+      "https://images.unsplash.com/photo-1637580690960-02f4673ba4c5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    title: "Qualified Professionals",
+    description:
+      "Top-notch customer service and treatment approach that is evidence based and proven to be successful",
+  },
+  {
+    class: "card2",
+    coverImage:
+      "https://images.pexels.com/photos/5473186/pexels-photo-5473186.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "Extensive Experience",
+    description:
+      "Over 20 years of combined experience, and extensive knowledge in many areas of physical and mental health",
+  },
+  {
+    class: "card3",
+    coverImage:
+      "https://images.pexels.com/photos/7772689/pexels-photo-7772689.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "Leading Technology",
+    description:
+      "Agile treatment techniques led by advanced technology to diagnose patient issues and concerns",
+  },
+  {
+    class: "card4",
+    coverImage:
+      "https://images.pexels.com/photos/4269205/pexels-photo-4269205.jpeg",
+    title: "Direct Billing", //look for ways to have the word "billing in a seperate line"
+    description:
+      "Direct billing to various public and private insurance providers in addition to all MVA and WCB Claims",
+  },
+];
