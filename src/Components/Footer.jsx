600d8cd324d7dbfa638d9656536ec4f7d20f65bd
Damian Roiz <damianx989@gmail.com>
2023-03-25 21:37:41 -0400

    smooth-scrolling to top

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -8,72 +8,130 @@ import { Link } from "wouter";
 
 const Footer = () => {
   return (
-<footer>
-  <div className="footer-sections">
-  <i className="fa fa-circle-chevron-up" aria-hidden="true"><a href="/"></a></i>
-    <div className="contact-info">
-      <img className="footer-logo" src="https://www.atriumphysiotherapy.com/images/atriumLogo.png" alt="Company Logo" />
-      <ul className="contact-list">
-        <li><i className="fa fa-map-marker-alt" aria-hidden="true"></i>
-          <p>340 Midpark Way SE - Suite 140 <span>Calgary, AB, T2X 1P1</span></p>
-        </li>
-        <li><i className="fa fa-envelope" aria-hidden="true"></i>
-          admin@atriumphysiotherapy.com</li>
-        <li><i className="fa fa-phone-alt" aria-hidden="true"></i>+1(403)-255 4461</li>
-        <li><i className="fa fa-fax" aria-hidden="true"></i>+1(403)-259 8776</li>
-      </ul>
-      <div className="social-media">
-        <a href="https://www.facebook.com/Atriumphysio/" target="_blank" rel="noopener">
-        <i className="fa-brands fa-facebook"></i>
-        </a>
-        <a href="https://www.instagram.com/atriumphysiotherapy/" target="_blank" rel="noopener">
-        <i className="fa-brands fa-instagram"></i>
-        </a>
-        <a href="https://www.linkedin.com/in/hala-basily-05872829/" target="_blank" rel="noopener">
-        <i className="fa-brands fa-linkedin"></i>
-        </a>
-        <a href="https://twitter.com/"target="_blank" rel="noopener">
-        <i className="fa-brands fa-twitter"></i>
-        </a>
+    <footer>
+      <div className="footer-sections"> 
+      <a href="#root" className="back-to-top"><i className="fa fa-circle-chevron-up" aria-hidden="true"></i></a>
+        <div className="contact-info">
+          <img
+            className="footer-logo"
+            src="https://www.atriumphysiotherapy.com/images/atriumLogo.png"
+            alt="Company Logo"
+          />
+          <ul className="contact-list">
+            <li>
+              <i className="fa fa-map-marker-alt" aria-hidden="true"></i>
+              <p>
+                340 Midpark Way SE - Suite 140 <span>Calgary, AB, T2X 1P1</span>
+              </p>
+            </li>
+            <li>
+              <i className="fa fa-envelope" aria-hidden="true"></i>
+              admin@atriumphysiotherapy.com
+            </li>
+            <li>
+              <i className="fa fa-phone-alt" aria-hidden="true"></i>+1(403)-255
+              4461
+            </li>
+            <li>
+              <i className="fa fa-fax" aria-hidden="true"></i>+1(403)-259 8776
+            </li>
+          </ul>
+          <div className="social-media">
+            <a
+              href="https://www.facebook.com/Atriumphysio/"
+              target="_blank"
+              rel="noopener"
+            >
+              <i className="fa-brands fa-facebook"></i>
+            </a>
+            <a
+              href="https://www.instagram.com/atriumphysiotherapy/"
+              target="_blank"
+              rel="noopener"
+            >
+              <i className="fa-brands fa-instagram"></i>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/hala-basily-05872829/"
+              target="_blank"
+              rel="noopener"
+            >
+              <i className="fa-brands fa-linkedin"></i>
+            </a>
+            <a href="https://twitter.com/" target="_blank" rel="noopener">
+              <i className="fa-brands fa-twitter"></i>
+            </a>
+          </div>
+        </div>
+
+        <div className="links">
+          <p>Links</p>
+          <ul>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/Services">Services</a>
+            </li>
+            <li>
+              <a href="/blog">Blog</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+            <li>
+              <a href="/covid19">Covid-19 Alert</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="hours-section">
+          <p>Operating Hours</p>
+          <ul>
+            <li>
+              Monday:<span>8:00 am - 7:00 pm</span>
+            </li>
+            <li>
+              Tuesday:<span>8:00 am - 5:00 pm</span>
+            </li>
+            <li>
+              Tuesday:<span>8:00 am - 5:00 pm</span>
+            </li>
+            <li>
+              Wednesday:<span>8:00 am - 7:00 pm</span>
+            </li>
+            <li>
+              Thursday:<span>8:00 am - 3:00 pm</span>
+            </li>
+            <li>
+              Friday:<span>8:00 am - 7:00 pm</span>
+            </li>
+            <li>
+              Saturday:<span>Closed</span>
+            </li>
+            <li>
+              Sunday:<span>Closed</span>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
 
-    <div className="links">
-      <p>Links</p>
-      <ul>
-        <li><a href="/about">About</a></li>
-        <li><a href="/Services">Services</a></li>
-        <li><a href="/blog">Blog</a></li>
-        <li><a href="/contact">Contact</a></li>
-        <li><a href="/covid19">Covid-19 Alert</a></li>
-      </ul>
-    </div>
-
-    <div className="hours-section">
-      <p>Operating Hours</p>
-      <ul>
-        <li>Monday:<span>8:00 am - 7:00 pm</span></li>
-        <li>Tuesday:<span>8:00 am - 5:00 pm</span></li>
-        <li>Tuesday:<span>8:00 am - 5:00 pm</span></li>
-        <li>Wednesday:<span>8:00 am - 7:00 pm</span></li>
-        <li>Thursday:<span>8:00 am - 3:00 pm</span></li>
-        <li>Friday:<span>8:00 am - 7:00 pm</span></li>
-        <li>Saturday:<span>Closed</span></li>
-        <li>Sunday:<span>Closed</span></li>
-      </ul>
-    </div>
-
-  </div>
-
-  <div className="footnote">
-    <div className="terms">
-      <Link className="footnote-link" href="#">Terms and Conditions</Link> | <Link className="footnote-link" href="#">Privacy Policy</Link>
-    </div>
-    <small className="copyright">Copyright ©2022 - All rights reserved.</small>
-  </div>
-</footer>
-  )
+      <div className="footnote">
+        <div className="terms">
+          <Link className="footnote-link" href="#">
+            Terms and Conditions
+          </Link>{" "}
+          |{" "}
+          <Link className="footnote-link" href="#">
+            Privacy Policy
+          </Link>
+        </div>
+        <small className="copyright">
+          Copyright ©2022 - All rights reserved.
+        </small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
-
--- src/Pages/Home/HomeSections.jsx
+++ src/Pages/Home/HomeSections.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Image, Stack, Box, Text, VStack, HStack } from "@chakra-ui/react";
 import { benefitCards, AboutUsContent } from "./HomeContent";
-import '../../style.css'
+import "../../style.css";
+import { red } from "@mui/material/colors";
 
 export const HomeCards = () => {
   return (
@@ -64,61 +65,73 @@ export const AboutUs = () => {
   return (
     <>
       <VStack>
-          <Box>
-            <Image maxWidth={1000} width="100%" height="100%" src={`src/assets/images/${AboutUsContent.image}`} alt="staff image" />
-          </Box>
-          <VStack maxWidth={1200}>
-          <Text p={30} fontFamily="Crimson Text, Georgia, Cambria, serif" fontSize="4em" color="#262B2F">
-              {AboutUsContent.title}
-            </Text>
-            <VStack fontSize="1.5rem" fontFamily="Fira Sans, Droid Sans, Century, sans-serif" color="#262B2F" pl={40} pr={40} spacing={20} lineHeight={1.3}>
-            <Text>
-              {AboutUsContent.subtitle1}
-            </Text>
-            <Text>
-              {AboutUsContent.subtitle2}
-            </Text>
-            </VStack>
-            <VStack paddingBlock="40" pl={60} pr={60} color="#1A2228" fontSize="1.25rem" textAlign="left" width="100%" fontFamily="Poppins, Roboto, Arial Narrow, sans-serif" spacing={20}>
-                <Text>
-                {AboutUsContent.paragraph1}
-                </Text>
-                <Text>
-                  {AboutUsContent.paragraph2}
-                </Text>
-                <Text width="inherit">
-                {AboutUsContent.paragraph3}
-                </Text>
-                <Text>
-                {AboutUsContent.paragraph4}
-                </Text>
-            </VStack>         
-            <Text pl={30} pr={30} pb={40} color="#1A2228" fontSize="1.25rem" textAlign="left" fontFamily="Poppins, Roboto, Arial Narrow, sans-serif">
-              {AboutUsContent.paragraph5}
-            </Text>
-          </VStack>         
+        <Box>
+          <Image
+            maxWidth={1200}
+            width="100%"
+            height="100%"
+            src={`src/assets/images/${AboutUsContent.image}`}
+            alt="staff image"
+          />
+        </Box>
+        <VStack maxWidth={1200} borderColor="red" borderWidth="10px">
+          <Text
+            p={30}
+            fontFamily="Crimson Text, Georgia, Cambria, serif"
+            fontSize="4em"
+            color="#262B2F"
+          >
+            {AboutUsContent.title}
+          </Text>
+          <VStack
+            maxW="1020px"
+            fontSize="1.5rem"
+            fontFamily="Fira Sans, Droid Sans, Century, sans-serif"
+            color="#262B2F"
+            pl={40}
+            pr={40}
+            spacing={20}
+            lineHeight={1.3}
+          >
+            <Text>{AboutUsContent.subtitle1}</Text>
+            <Text>{AboutUsContent.subtitle2}</Text>
+          </VStack>
+          <VStack
+            paddingBlock="40"
+            color="#1A2228"
+            fontSize="1.25rem"
+            textAlign="left"
+            maxW="70%"
+            fontFamily="Poppins, Roboto, Arial Narrow, sans-serif"
+            spacing={20}
+          >
+            <Text>{AboutUsContent.paragraph1}</Text>
+            <Text>{AboutUsContent.paragraph2}</Text>
+            <Text width="inherit">{AboutUsContent.paragraph3}</Text>
+            <Text>{AboutUsContent.paragraph4}</Text>
+            <Text>{AboutUsContent.paragraph5}</Text>
+          </VStack>
+        </VStack>
       </VStack>
-    </>  
+    </>
   );
 };
 
 export const MissionBanner = () => {
   return (
     <div className="banner-container">
-        <div className="text-banner">
-          <h2>We Provide High Quality Services</h2>
-          <p>
-            We believe that in order to maintain a pain free body it is
-            necessary to understand a person's specific injury and prevent
-            situations that might exacerbate or aggravate a condition.
-          </p>
-        </div>
-        <div>
-          <button className="appointment-btn">
-            Book Your Appoinment
-          </button>
-        </div>
+      <div className="text-banner">
+        <h2>We Provide High Quality Services</h2>
+        <p>
+          We believe that in order to maintain a pain free body it is necessary
+          to understand a person's specific injury and prevent situations that
+          might exacerbate or aggravate a condition.
+        </p>
       </div>
+      <div>
+        <button className="appointment-btn">Book Your Appoinment</button>
+      </div>
+    </div>
   );
 };
 
