8db18118b27d3991a86b0452c368de9ba54e210e
Damian Roiz <damianx989@gmail.com>
2023-05-21 15:35:56 -0400

    links resolved

--- src/Pages/Blog/client/BlogMenu.jsx
+++ src/Pages/Blog/client/BlogMenu.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 const BlogMenu = () => {
   return (
     <nav className="blog-menu">
-      <Link to="/login" className="blog-link">
+      <Link to="/Blog/login" className="blog-link">
         Login
       </Link>
-      <Link to="/register" className="blog-link">
+      <Link to="/Blog/register" className="blog-link">
         Register
       </Link>
     </nav>
