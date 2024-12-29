4aff738a792820485ba142f417ab89ca7ff93ed0
Damian Roiz <damianx989@gmail.com>
2024-12-29 11:26:39 -0500

    Add Supabase integration and update styles: import global styles, add Supabase client, and enhance Header and Navbar design on smaller viewport

--- src/Pages/Dashboard/ui/Header.jsx
+++ src/Pages/Dashboard/ui/Header.jsx
@@ -2,10 +2,10 @@ import styled from 'styled-components';
 import Logo from '../../../Components/Logo';
 
 const StyledHeader = styled.header`
-  background-color: var(--c-grey-200);
+  background-color: var(--c-grey-100);
   padding: 1.2rem 4.8rem;
   grid-column: 1 / -1;
-  border-bottom: 1px solid var(--c-grey-300);
+  border-bottom: 1px solid var(--c-grey-500);
 `;
 
 function Header() {
