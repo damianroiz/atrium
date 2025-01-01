7ab11c6685fd08a06f8960bb6c0d8ec90010ed16
Damian Roiz <damianx989@gmail.com>
2025-01-01 15:01:29 -0500

    posts being fetched correctly to post page, updated .env variuables

--- src/Pages/Dashboard/Posts.jsx
+++ src/Pages/Dashboard/Posts.jsx
@@ -1,11 +1,18 @@
-import Row from "./ui/Row"
-import Heading from "./ui/Heading"
+import Row from './ui/Row';
+import Heading from './ui/Heading';
+import { useEffect } from 'react';
+import { getPosts } from '../../services/apiPosts';
 
-export default function Posts() {
+function Posts() {
+  useEffect(() => {
+    getPosts().then((posts) => console.log(posts));
+  });
   return (
     <Row type="horizontal">
       <Heading as="h1">Posts</Heading>
       <p>test</p>
     </Row>
-  )
+  );
 }
+
+export default Posts;
