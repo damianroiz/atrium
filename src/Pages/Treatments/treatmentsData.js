c828067aa110b98a997f29ee8067b59160bfb4de
Damian Roiz <damianx989@gmail.com>
2025-02-19 17:27:24 -0500

    added supabase url images

--- src/Pages/Treatments/treatmentsData.js
+++ src/Pages/Treatments/treatmentsData.js
@@ -2,7 +2,8 @@ export const treatmentsData = [
   {
     id: 1,
     title: 'Physiotherapy',
-    image: 'https://atriumphysiotherapy.com/images/stretching.jpg',
+    image:
+      'https://tzxlnpsumiqvxelgwwlf.supabase.co/storage/v1/object/public/web-images//stretching.jpg',
     description: [
       'Our physiotherapists are highly-educated experts in physical function, movement and mobility. They have advanced knowledge of how the human body moves and what stops it moving and use specialized hands-on treatment to restore, maintain and maximize optimal function and quality of life.',
       'They assist, diagnose and treat physical symptoms and limited movement caused by injury, aging, disability, or medical condition, They help patients understand what is causing their condition, and work with them to restore, maintain and maximize movement, flexibility and physical independence. Through customized treatment plans that help patients take back control. They teach patients how to restore, maintain and/or maximize movement, reduce pain, and manage any chronic symptoms',
@@ -12,7 +13,8 @@ export const treatmentsData = [
   {
     id: 2,
     title: 'Functional Dry Needling',
-    image: 'https://atriumphysiotherapy.com/images/ims1.jpg',
+    image:
+      'https://tzxlnpsumiqvxelgwwlf.supabase.co/storage/v1/object/public/web-images//ims1.jpg',
     description: [
       'Dry Needling is a valuable, effective and efficient adjunct treatment to inactivate myofascial trigger points. It is an invasive procedure in which a thin solid filament sterile needle is used.',
       'Dry Needling involves insertion and repetitive manipulation of the needle in the myofascial trigger point. The purpose of this technique is to inactivate the myofascial trigger points by producing a local twitch response. This local twitch response then releases the shortened bands of muscle fibers.',
@@ -22,7 +24,8 @@ export const treatmentsData = [
   {
     id: 3,
     title: 'Acupuncture',
-    image: 'https://atriumphysiotherapy.com/images/acupuncture.jpg',
+    image:
+      'https://tzxlnpsumiqvxelgwwlf.supabase.co/storage/v1/object/public/web-images//acupuncture.jpg',
     description: [
       'Acupuncture is an ancient form of Chinese medicine involving the insertion of ultra-fine needles into specific points of the skin. These points of energy are located along 14 meridians, or channels, that cover the entire body. Using modern techniques, contemporary medical acupuncture is able to precisely stimulate nerves to modulate pain and other dysfunctions.',
       'Whether there is a presence of a medical condition or not, everybody can benefit from acupuncture due to its homeostatic effects, which help the body regain balance and function at a more optimal level.',
@@ -31,7 +34,8 @@ export const treatmentsData = [
   {
     id: 4,
     title: 'Graston Technique',
-    image: 'https://atriumphysiotherapy.com/images/Graston.jpg',
+    image:
+      'https://tzxlnpsumiqvxelgwwlf.supabase.co/storage/v1/object/public/web-images//Graston.jpg',
     description: [
       'Graston technique is a form of instrument-assisted or augmented soft tissue mobilization (ASTM) that enables practitioners to improve scar tissue fascial restrictions and range of motion. The theory behind this IASTM technique is that by using a tool to introduce microtrauma into an area of excessive scarring and/or soft tissue fibrosis, an inflammatory response will occur. In a 2017 Journal of Exercise Rehabilitation study, the authors describe that “Such inflammation restarts the healing process by removing the scar tissue and releasing adhesions, while also increasing blood and nutrient supply to the injured area and migration of fibroblasts.” (1b)',
     ],
@@ -39,7 +43,8 @@ export const treatmentsData = [
   {
     id: 5,
     title: 'Triton Spinal Decompression',
-    image: 'https://atriumphysiotherapy.com/images/tritonSpinal.png',
+    image:
+      'https://tzxlnpsumiqvxelgwwlf.supabase.co/storage/v1/object/public/web-images//spinal-decompression.jpg',
     description: [
       'Spinal Decompression is a non-surgical and drug-free answer for disc related problems of the lumbar or cervical spine. Many people across the country and around the world have found relief from the pain associated with herniated discs, bulging discs, facet syndrome, degenerative joint disease, pinched nerves, and other spinal afflictions from decompression therapy. Spinal Disc Decompression uses computer-aided technology to apply gentle, non-surgical decompression to your spine which increases circulation into the spinal discs and joints, thus helping to relieve the symptoms that cause pain and dysfunction.',
     ],
@@ -48,7 +53,7 @@ export const treatmentsData = [
     id: 6,
     title: 'Laser Therapy',
     image:
-      'https://atriumphysiotherapy.com/wp-content/uploads/2023/11/pasted-image-1.png',
+      'https://tzxlnpsumiqvxelgwwlf.supabase.co/storage/v1/object/public/web-images//laser-therapy.jpg',
     description: [
       'Laser Therapy uses red and infrared light for the relief of pain, to accelerate healing and decrease inflammation. When the light source is placed against the skin, the photons penetrate several centimeters and get absorbed by the mitochondria, the energy producing part of a cell. This energy fuels many positive physiological responses resulting in the restoration of normal cell morphology and function. Laser Therapy has been successfully used to treat a broad range of medical conditions, including musculoskeletal problems, arthritis, sports injuries, post-surgical wounds, diabetic ulcers and dermatological conditions.',
     ],
@@ -57,7 +62,7 @@ export const treatmentsData = [
     id: 7,
     title: 'K-Tape',
     image:
-      'https://atriumphysiotherapy.com/wp-content/uploads/2023/11/pasted-image-2.png',
+      'https://tzxlnpsumiqvxelgwwlf.supabase.co/storage/v1/object/public/web-images//k-tape.jpeg',
     description: [
       'KT Tape® is applied along muscles, ligaments, and tendons (soft tissue) to provide a lightweight, external support that helps you remain active while recovering from injuries.',
       'When an area of the body is injured through impact or over-use, the lymphatic fluid builds up causing inflammation and swelling. This accumulation of lymphatic fluids may cause increased pressure on muscles and tissue which can cause significant discomfort or pain',
@@ -67,7 +72,8 @@ export const treatmentsData = [
   {
     id: 8,
     title: 'Massage Therapy',
-    image: 'https://atriumphysiotherapy.com/images/massage_wide.jpg',
+    image:
+      'https://tzxlnpsumiqvxelgwwlf.supabase.co/storage/v1/object/public/web-images//massage_wide.jpg',
     description: [
       'Massage Therapy involves gentle hands-on manipulation of the soft tissues of your body. Therapeutic massage focuses on your muscles, connective tissue, tendons, ligaments and joints. Atrium Physiotherapy Registered Massage Therapists manipulate your muscles and other soft tissues of the body with varying degrees of pressure and movement. This helps to relieve pain and tension. If done soon enough after accidents involving trauma and injury, massage can greatly reduce the development of painful muscular patterning.',
     ],
@@ -76,7 +82,7 @@ export const treatmentsData = [
     id: 9,
     title: 'Cupping Therapy',
     image:
-      'https://atriumphysiotherapy.com/wp-content/uploads/2023/11/Screenshot-2023-11-16-at-17-43-05-Fwd-Website-Contents-jw763444@gmail.com-Gmail.png',
+      'https://tzxlnpsumiqvxelgwwlf.supabase.co/storage/v1/object/public/web-images//cupping-therapy.jpg',
     description: [
       'Hot cupping is an ancient form of alternative medicine in which a therapist puts special cups on your skin for a few minutes to create suction. People get it for many purposes, including to help with pain, inflammation, blood flow, relaxation and well-being, and as a type of deep-tissue massage.',
     ],
@@ -84,7 +90,8 @@ export const treatmentsData = [
   {
     id: 10,
     title: 'Hot stone massage',
-    image: 'https://atriumphysiotherapy.com/images/hotstone2.jpeg',
+    image:
+      'https://tzxlnpsumiqvxelgwwlf.supabase.co/storage/v1/object/public/web-images//hot-stone-massage.jpeg',
     description: [
       'Combining hot stone protocols with a full body massage provides a very healing and effective experience. The hot stones also expand blood vessels, which encourages blood flow throughout the body. The hot stones have a sedative effect that can relieve chronic pain, reduce stress and promote deep relaxation.',
       'A hot stone massage is a type of massage therapy. It’s used to help you relax and ease tense muscles and damaged soft tissues throughout your body.',
@@ -95,7 +102,7 @@ export const treatmentsData = [
     id: 11,
     title: 'Custom Crafted Orthotics',
     image:
-      'https://atriumphysiotherapy.com/wp-content/uploads/2023/11/Screenshot-2023-11-16-at-17-43-26-Fwd-Website-Contents-jw763444@gmail.com-Gmail.png',
+      'https://tzxlnpsumiqvxelgwwlf.supabase.co/storage/v1/object/public/web-images//custom-orthotics.jpg',
     description: [
       'An orthotic is a supportive device which is placed inside footwear, to change the mechanical function of the foot. It works dynamically during weight-bearing activities like walking, running and standing. Ideally, it should provide full, custom and corrected arch contact so that the foot works in a biomechanically correct way. At Atrium Physiotherapy we offer Custom Crafted Orthotics, calibrated to flex for your weight and foot type.',
     ],
@@ -104,7 +111,7 @@ export const treatmentsData = [
     id: 12,
     title: 'MVA (Motor Vehicle Accident)',
     image:
-      'https://atriumphysiotherapy.com/wp-content/uploads/2023/11/Screenshot-2023-11-16-at-17-43-32-Fwd-Website-Contents-jw763444@gmail.com-Gmail.png',
+      'https://tzxlnpsumiqvxelgwwlf.supabase.co/storage/v1/object/public/web-images//mva.png',
     description: [
       "A personal injury from auto accident trauma is common, but sometimes severe. An injury caused by Motor Vehicle Accident may result in lost work time, and cause difficulties in dealing with day-to-day responsibilities. After you've been injured in a crash, it's vital to get physiotherapy as quickly as possible. Many times, the effects of an auto injury are delayed or subtle. Seek medical advice as quickly as possible to ensure you are adequately treated and protected for the future.",
     ],
@@ -112,7 +119,8 @@ export const treatmentsData = [
   {
     id: 13,
     title: 'Workplace Injury Rehabilitation',
-    image: 'https://atriumphysiotherapy.com/images/wcb.jpg',
+    image:
+      'https://tzxlnpsumiqvxelgwwlf.supabase.co/storage/v1/object/public/web-images//worker-injury.jpg',
     description: [
       'Workplace injuries are a common problem. Atrium Physiotherapy offers programs that provide workplace injury rehabilitation.',
       'Atrium Physiotherapy focus on an active injury approach to promote healing, improve active range of motion, restore muscle strength and endurance and restore function, to facilitate early and safe return to work. Whether your workplace injury has occurred from an accident or from overuse, you will find Atrium Physiotherapy programs helpful',
@@ -121,7 +129,8 @@ export const treatmentsData = [
   {
     id: 14,
     title: 'Private Insurance Claims',
-    image: 'https://atriumphysiotherapy.com/images/ins.jpg',
+    image:
+      'https://tzxlnpsumiqvxelgwwlf.supabase.co/storage/v1/object/public/web-images//back-pain.jpg',
     description: [
       "For individuals who has extended health insurance through their employer or their spouse's employer , we can do direct on-line billing for Private insurance companies such as Alberta Blue Cross ,and Great West Life.",
       'If your plan is based on re-embarrassment , we do accept Debit, Visa and Master Card . We will provide you with an official receipt to submit to your private insurance .',
@@ -143,7 +152,7 @@ export const treatmentsData = [
     id: 16,
     title: 'Vestibular Rehabilitations',
     image:
-      'https://atriumphysiotherapy.com/wp-content/uploads/2023/11/Screenshot-2023-11-16-at-17-43-38-Fwd-Website-Contents-jw763444@gmail.com-Gmail.png',
+      'https://tzxlnpsumiqvxelgwwlf.supabase.co/storage/v1/object/public/web-images//vestibular-rehab.png',
     description: [
       'Benign Paroxysmal Positional Vertigo (BPPV), an inner ear problem, is the most common cause of vertigo. We can normally correct the condition in 1-3 treatments.',
       'Vestibular rehabilitation is a proven treatment for dizziness and balance problems and is highly effective for all age groups.',
@@ -154,7 +163,7 @@ export const treatmentsData = [
     id: 17,
     title: 'Pelvic Floor Physiotherapy',
     image:
-      'https://atriumphysiotherapy.com/wp-content/uploads/2023/11/Screenshot-2023-11-16-at-17-43-44-Fwd-Website-Contents-jw763444@gmail.com-Gmail.png',
+      'https://tzxlnpsumiqvxelgwwlf.supabase.co/storage/v1/object/public/web-images//pelvic-floor-therapy.png',
     description: [
       "Pelvic floor physiotherapy help women rehabilitate their pelvic floor muscles. These muscles can be weakened by childbirth, surgery, heavy lifting, being overweight, constipation or menopause. The pelvic floor muscles support the womb (uterus), bladder and bowel (colon), forming a 'sling' from the pubic bone at the front to the tailbone at the back. If the muscles are weak, this can affect bladder and bowel control, resulting in incontinence (leakage) or prolapse.",
     ],
